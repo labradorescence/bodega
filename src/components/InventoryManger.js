2fd53c88ce8fe85d7dfd668690361231e8de3abe
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-01-24 23:02:27 -0500

    revieww

--- src/components/InventoryManger.js
+++ src/components/InventoryManger.js
@@ -3,8 +3,6 @@ import CurrentInventoryList from "./CurrentInventoryList";
 import ReorderInventoryList from "./ReorderInventoryList"
 const url = "http://localhost:8001/inventory"
 
-// /inventory
-// /invetory/:id
 
 function InventoryManager() {
 
@@ -25,29 +23,13 @@ function InventoryManager() {
       setReorderInventory(reorderInventory.filter((oneItem) => oneItem.id !== item.id))
     }
 
-    function removeItemfromStateArray(itemToRemove, stateArray, setStateFunction){
-        const reorderItemIndex = stateArray.findIndex(item => item.id === itemToRemove.id)
-
-        console.log(reorderItemIndex)
-
-        if( reorderItemIndex >= 0){
-            const arrayCopy = [...stateArray]
-            arrayCopy.splice(reorderItemIndex, 1);
-            console.log(arrayCopy)
-
-            setStateFunction(arrayCopy)
-        } else {
-            console.log("couldn't remove item")
-        }
-    }
 
     function onDelete(item){
         fetch(`${url}/${item.id}`,{
             method: "DELETE"
         })
-
-        removeItemfromStateArray(item, reorderInventory, setReorderInventory)
-        removeItemfromStateArray(item, inventory, setInventory)
+        setReorderInventory(reorderInventory.filter((oneItem) => oneItem.id !== item.id))
+        setInventory(inventory.filter((oneItem) => oneItem.id !== item.id))
     }
 
     
